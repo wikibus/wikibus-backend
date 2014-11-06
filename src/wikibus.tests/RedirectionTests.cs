b9a20f405ee820c4db1dbcca99b84a75bf5fb706
tpluscode <tomasz@t-code.pl>
2014-11-06 21:24:47 +0100

    first test for purl-like redirect

---
+++ src/wikibus.tests/RedirectionTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+using Nancy;
+using Nancy.Responses.Negotiation;
+using Nancy.Testing;
+
+namespace wikibus.tests
+{
+    [TestFixture]
+    public class RedirectionTests
+    {
+        private Browser _browser;
+
+        [SetUp]
+        public void Setup()
+        {
+            _browser = new Browser(c => c.Assembly("wikibus.purl.nancy"));
+        }
+
+        [Test]
+        public void Should_redirect_rdf_requests_to_document(
+            [ValueSource("PathsToRedirect")] string path,
+            [ValueSource("RdfMediaTypes")] MediaRange media)
+        {
+            // when
+            var response = _browser.Get(path, context => context.Accept(media));
+
+            // then
+            response.StatusCode.Should().Be(HttpStatusCode.SeeOther);
+            response.Headers["Location"].Should().Be(new Uri(new Uri("http://wikibus.org/"), path).ToString());
+        }
+
+        private IEnumerable<string> PathsToRedirect()
+        {
+            yield return "/brochure/";
+        }
+
+        private IEnumerable<MediaRange> RdfMediaTypes()
+        {
+            yield return new MediaRange("text/turtle");
+        }
+    }
+}
