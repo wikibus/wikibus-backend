75296b6ee688b4b997cc827643134591f44382b6
tpluscode <tpluscode@users.noreply.github.com>
2014-11-16 21:58:10 +0100

    more wikibus.web

---
+++ src/wikibus.web/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using VDS.RDF;
+using VDS.RDF.Query;
+
+namespace wikibus.web
+{
+    public class Bootstrapper : Nancy.DefaultNancyBootstrapper
+    {
+        protected override void ConfigureApplicationContainer(Nancy.TinyIoc.TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+
+            container.Register<ISparqlQueryProcessor>(new LeviathanQueryProcessor(new TripleStore()));
+        }
+    }
+}
