3204c005b6f37b984633c4b20c47f07f80478097
tpluscode <tpluscode@users.noreply.github.com>
2014-11-17 18:06:38 +0100

    fix common assembly stylecop problems

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,7 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Tomasz Pluskiewicz T+Code")]
 [assembly: AssemblyProduct("wikibus.org")]
 [assembly: AssemblyCopyright("Copyright © Tomasz Pluskiewicz 2014")]
-
