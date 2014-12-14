5276c7f8f56836a8a8926cd3ba2d92be0f5a1eaf
tpluscode <tpluscode@users.noreply.github.com>
2014-12-14 12:20:41 +0100

    add ELMAH

---
+++ src/wikibus.nancy/ElmahStartup.cs
@@ -0,0 +1,20 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Elmah;
+
+namespace wikibus.nancy
+{
+    /// <summary>
+    /// Wires logging framework with the application
+    /// </summary>
+    public class ElmahStartup : IApplicationStartup
+    {
+        /// <summary>
+        /// Perform logging framework initialization task
+        /// </summary>
+        public void Initialize(IPipelines pipelines)
+        {
+            Elmahlogging.Enable(pipelines, "elmah", new string[0], new[] { HttpStatusCode.NotFound, HttpStatusCode.InsufficientStorage, });
+        }
+    }
+}
