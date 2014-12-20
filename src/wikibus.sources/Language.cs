232532d81265512bb7b6e7188cdb6c5b2fda4627
tpluscode <tpluscode@users.noreply.github.com>
2014-12-20 12:57:11 +0100

    fix Language deserialization

--- src/wikibus.sources/Language.cs
+++ src/wikibus.sources/Language.cs
@@ -7,13 +7,70 @@ namespace wikibus.sources
     /// Represents a language
     /// </summary>
     [JsonConverter(typeof(LexvoIso639LanguageConverter))]
-    public class Language : CultureInfo
+    public class Language
     {
+        private readonly CultureInfo _cultureInfo;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="Language"/> class.
         /// </summary>
-        public Language(string name) : base(name)
+        public Language(string name) : this(new CultureInfo(name))
         {
         }
+
+        private Language(CultureInfo cultureInfo)
+        {
+            _cultureInfo = cultureInfo;
+        }
+
+        /// <summary>
+        /// Gets the language's name
+        /// </summary>
+        public string Name
+        {
+            get { return _cultureInfo.Name; }
+        }
+
+        #region Generated Code
+        public static bool operator ==(Language left, Language right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Language left, Language right)
+        {
+            return !Equals(left, right);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            return Equals((Language)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _cultureInfo.GetHashCode();
+        }
+
+        protected bool Equals(Language other)
+        {
+            return _cultureInfo.Equals(other._cultureInfo);
+        }
+        #endregion
     }
 }
