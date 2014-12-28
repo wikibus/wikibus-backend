806745224eedc64db37387ae7144b39a083476b5
tpluscode <tpluscode@users.noreply.github.com>
2014-12-28 14:27:31 +0100

    rdf startup

---
+++ src/wikibus.nancy/RdfStartup.cs
@@ -0,0 +1,19 @@
+﻿using Nancy.Bootstrapper;
+using Nancy.RDF;
+
+namespace wikibus.nancy
+{
+    /// <summary>
+    /// Sets-up RDF serialization options
+    /// </summary>
+    public class RdfStartup : IApplicationStartup
+    {
+        /// <summary>
+        /// Sets Rdf response processing options
+        /// </summary>
+        public void Initialize(IPipelines pipelines)
+        {
+            ////Nancy.RDF.RdfResponses.SetDefaultSerialization(pipelines, RdfSerialization.Turtle);
+        }
+    }
+}
