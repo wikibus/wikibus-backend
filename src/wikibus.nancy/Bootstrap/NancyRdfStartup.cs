30410ddc706216d177774161ffd96c96a81e179c
tpluscode <tpluscode@users.noreply.github.com>
2014-12-30 15:38:47 +0100

    rename Nancy.RDF startup

--- src/wikibus.nancy/Bootstrap/NancyRdfStartup.cs
+++ src/wikibus.nancy/Bootstrap/NancyRdfStartup.cs
@@ -1,14 +1,24 @@
 ﻿using Nancy.Bootstrapper;
 using Nancy.RDF;
 using Nancy.RDF.Responses;
+using VDS.RDF;
 
 namespace wikibus.nancy
 {
     /// <summary>
     /// Sets-up RDF serialization options
     /// </summary>
-    public class RdfStartup : IApplicationStartup
+    public class NancyRdfStartup : IApplicationStartup
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NancyRdfStartup"/> class.
+        /// </summary>
+        public NancyRdfStartup(INamespaceMapper mapper)
+        {
+            mapper.Import(new NamespaceMapper());
+            mapper.AddNamespace("");
+        }
+
         /// <summary>
         /// Sets Rdf response processing options
         /// </summary>
