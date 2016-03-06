4d74e60d378283f5d400cf2a9bb0042e597b6755
tpluscode <tpluscode@users.noreply.github.com>
2016-03-06 13:04:59 +0100

    purl to owin

---
+++ src/wikibus.org/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using wikibus.org;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace wikibus.org
+{
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.UseNancy();
+        }
+    }
+}
