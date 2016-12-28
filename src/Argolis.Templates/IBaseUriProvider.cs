1e7433fde1add280f7979cf7bffae1f3b9a670a3
tpluscode <tpluscode@users.noreply.github.com>
2016-12-28 22:08:57 +0100

    remove spike code

--- src/Argolis.Templates/IBaseUriProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Argolis.Templates
-{
-    public interface IBaseUriProvider
-    {
-        string BaseUri { get; }
-    }
-}
--- src/Argolis.Templates/TemplateAttributeBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Argolis.Templates
-{
-    public abstract class TemplateAttributeBase : Attribute
-    {
-        public string Template { get; protected set; }
-    }
-}
